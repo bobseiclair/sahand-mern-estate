b650ccf35705a6675d1493ad0d2d127512d3b736
bobseiclair <bobseinc@gmail.com>
2023-10-10 22:50:22 +0000

    create a middleware to handle error

--- api/index.js
+++ api/index.js
@@ -18,3 +18,13 @@ app.listen(port, () => console.log(`Server en marche sur le port ${port}`))
 
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
