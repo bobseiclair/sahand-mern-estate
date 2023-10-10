39febc3e2b2c578800bbff1f2b954757b43b7349
bobseiclair <bobseinc@gmail.com>
2023-10-10 22:21:48 +0000

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 import dotenv from 'dotenv'
 dotenv.config()
@@ -11,7 +12,9 @@ const port =  3000 || process.env.PORT
 connectDB()
 
 const app = express()
+app.use(express.json())
 
 app.listen(port, () => console.log(`Server en marche sur le port ${port}`))
 
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
